fcc2cdf6a2a40d4fa71f12cc71d47504f7672a87
wiolaSzczepanik <wiolaszcz@gmail.com>
2017-10-31 16:29:23 +0100

    Add function_10.py

--- function_10.py
+++ function_10.py
@@ -1,3 +1,27 @@
-"""Define a function overlapping() that takes two lists and returns True if they have at least one member in common,
-False otherwise. You may use your is_member() function, or the in operator, but for the sake of the exercise, you
+"""Define a function overlapping() that takes two lists and returns True if
+they have at least one member in common, False otherwise. You may use your
+is_member() function, or the in operator, but for the sake of the exercise, you
 should (also) write it using two nested for-loops."""
+
+def overlapping(list_1, list_2):
+    for item in list_1:
+        for ele in list_2:
+            if ele == item:
+                return True
+    return False
+
+
+def test(list_1, list_2, expected_result):
+    actual_result = overlapping(list_1, list_2)
+    if actual_result == expected_result:
+        print("PASS")
+    else:
+        print("FAIL. ")
+
+
+def main():
+    test(['a','b'],['b','c'], True)
+    test([1,2,3,4,],[3,4,5,6], True)
+
+
+main()
--- function_8.py
+++ function_8.py
@@ -1,2 +1,3 @@
-"""Define a function is_palindrome() that recognizes palindromes (i.e. words that look the same written backwards).
+"""Define a function is_palindrome() that recognizes palindromes
+(i.e. words that look the same written backwards).
 For example, is_palindrome("radar") should return True."""
